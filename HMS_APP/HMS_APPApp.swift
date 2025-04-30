21fa7dc558239e584f368733b0838953aec3c77f
Prasanjit-4 <prasanjitpanda8@gmail.com>
2025-04-30 23:47:52 +0530

    Initial Commit

---
+++ HMS_APP/HMS_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMS_APPApp.swift
+//  HMS_APP
+//
+//  Created by Prasanjit Panda on 30/04/25.
+//
+
+import SwiftUI
+
+@main
+struct HMS_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
