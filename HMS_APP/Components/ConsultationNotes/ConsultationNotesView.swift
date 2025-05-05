4274f984fffe551e56c4328812583f354da89824
rudra-02 <rudrapruthi@gmail.com>
2025-05-05 12:53:17 +0530

    test

--- HMS_APP/Components/ConsultationNotes/ConsultationNotesView.swift
+++ HMS_APP/Components/ConsultationNotes/ConsultationNotesView.swift
@@ -279,9 +279,10 @@ struct ConsultationNotesView: View {
             
             if let data = document.data() {
                 // Parse appointment data
-                self.patientId = data["patientId"] as? String ?? ""
-                self.doctorId = data["doctorId"] as? String ?? ""
-                self.doctorName = data["doctorName"] as? String ?? ""
+                self.patientId = data["patId"] as? String ?? ""
+                self.doctorId = data["docId"] as? String ?? ""
+                self.doctorName = data["docName"] as? String ?? ""
+                self.
                 
                 // Parse date
                 if let dateString = data["date"] as? String {
